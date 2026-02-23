79a952a724cdcd242d22fde5c5c40f855d0e691e
Ishika Patel <ishikapatel1364@gmail.com>
2026-02-23 08:57:47 +0530

    feat(auth): enhance registration form with validation rules

--- apps/web/src/app/features/auth/register/register.ts
+++ apps/web/src/app/features/auth/register/register.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { ReactiveFormsModule, FormGroup, FormControl, FormControlName } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  FormControlName,
+  Validators,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { validate } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-register',
@@ -10,10 +17,22 @@ import { CommonModule } from '@angular/common';
 })
 export class Register {
   registerForm = new FormGroup({
-    name: new FormControl(''),
-    username: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(20),
+    ]),
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(20),
+    ]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+    ]),
   });
 
   handleRegister() {}
