7edf3225d25d8e845a06be41121540eff3754289
Ishika Patel <ishikapatel1364@gmail.com>
2026-03-03 12:35:26 +0530

    feat: implement LinkService for managing link creation

---
+++ apps/web/src/app/core/services/link-service.ts
@@ -0,0 +1,19 @@
+import { environment } from '@/environment/environment';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CreateLinkBody, LinkResponse } from '@linktree/validation';
+import { Link } from '@/app/features/link/link';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LinkService {
+  private readonly API_URL: string = `${environment.apicall}/link`;
+
+  constructor(private http: HttpClient) {}
+
+  createTask(payload: CreateLinkBody): Observable<LinkResponse> {
+    return this.http.post<LinkResponse>(`${this.API_URL}/`, payload);
+  }
+}
