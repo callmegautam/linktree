4ffc66f5febb12832d45de31c841c70924b740d8
Gautam Suthar <iamgautamsuthar@outlook.com>
2026-03-06 00:01:08 +0530

    feat: integrate dynamic analytics data into dashboard components

--- apps/web/src/app/features/admin/dashboard/dashboard.ts
+++ apps/web/src/app/features/admin/dashboard/dashboard.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { AnalyticsService } from '@/app/core/services/admin/analytics-service';
+import { DatePipe, DecimalPipe } from '@angular/common';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { AnalyticsResponse } from '@linktree/validation';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [DatePipe, DecimalPipe],
   templateUrl: './dashboard.html',
 })
-export class Dashboard {}
+export class Dashboard {
+  analytics: AnalyticsResponse | null = null;
+
+  constructor(
+    private analyticsService: AnalyticsService,
+    private cd: ChangeDetectorRef,
+  ) {}
+
+  ngOnInit(): void {
+    this.analyticsService.getAnalytics().subscribe((res) => {
+      if (res && res.data) {
+        this.analytics = res.data;
+        console.log(this.analytics);
+        this.cd.detectChanges();
+      }
+    });
+  }
+}
